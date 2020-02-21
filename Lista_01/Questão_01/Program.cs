4109beb570fa8b828a3f0939519d68c29c9b46da
Marcos Vinicius Maia da Silva <vinicius.maia@academico.ifrn.edu.br>
2020-02-21 15:47:00 -0300

    Versão 01

---
+++ Lista_01/Questão_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Bem-vindo ao C#, {Name}");
+            Console.ReadLine();
+        }
+    }
+}
